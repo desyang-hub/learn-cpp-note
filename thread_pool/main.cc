7b1639f84c3a5f7e2b0aaf4675c2e9f7c2507b20
desyang <desyang@qq.com>
2026-05-07 21:31:53 +0800

    线程池标准实现

---
+++ thread_pool/main.cc
@@ -0,0 +1,40 @@
+#include "thread_pool.h"
+
+#include <iostream>
+
+using namespace std;
+
+int add_range(int a, int b) {
+    int sum = 0;
+    for (int i = a; i <= b;  ++i) sum+= i;
+    return sum;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    thread_pool pool(8);
+
+    int num = 1e6;
+
+    std::vector<std::future<int>> results;
+    results.reserve(num);
+
+    for (int i = 0; i < num; ++i) {
+        results.emplace_back(pool.enqueue(add_range, 0, i));
+        // add_range(0, i);
+        // int d = i + i + 1;
+    }
+
+    // for (auto& f : results) {
+    //     cout << f.get() << std::endl;
+    // }
+
+    
+    return 0;
+}
+
+
+int add(int a, int b) {
+    return a + b;
+}
