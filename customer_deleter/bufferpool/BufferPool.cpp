930174f21c8d3ee5cc11bdf1bb25354e9a8b1ce0
desyang <desyang@qq.com>
2026-07-20 11:04:03 +0800

    feat: 修正BufferPool

--- customer_deleter/bufferpool/BufferPool.cpp
+++ customer_deleter/bufferpool/BufferPool.cpp
@@ -8,7 +8,7 @@ BufferPool::BufferPool(int buffer_size, int element_bytes_size) : is_close_(fals
     data_ = std::unique_ptr<char[]>(new char[total_bytes_]);
 
     for (int i = 0; i < buffer_size; ++i) {
-        que_.push(data_.get() + i * buffer_size + element_bytes_size);
+        que_.push(data_.get() + i * element_bytes_size);
     }
 }
 
